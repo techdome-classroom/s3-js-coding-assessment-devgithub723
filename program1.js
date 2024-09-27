bd8d8a894a49aabcdd1ccf5e996544bdb7c22779
devgithub723 <devs63849@gmail.com>
2024-09-27 15:38:38 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,11 +1,44 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// var isValid = function(s) {
     
+// };
+
+// module.exports = { isValid };
+
+
+var isValid = function(s) {
+    // Stack to keep track of opening brackets
+    let stack = [];
+    
+    // Map to associate closing brackets with corresponding opening ones
+    const bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    
+    // Iterate through each character in the string
+    for (let char of s) {
+        // If it's a closing bracket
+        if (char in bracketMap) {
+            // Pop the top of the stack if it's not empty, otherwise assign a dummy value '#'
+            let topElement = stack.length === 0 ? '#' : stack.pop();
+            
+            // If the top element doesn't match the corresponding opening bracket, return false
+            if (bracketMap[char] !== topElement) {
+                return false;
+            }
+        } else {
+            // It's an opening bracket, push it onto the stack
+            stack.push(char);
+        }
+    }
+    
+    // If the stack is empty, all brackets are properly closed
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
