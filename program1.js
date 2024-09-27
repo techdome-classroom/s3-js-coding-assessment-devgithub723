8205e141db3a5012f35ac8f11cea017c09c1c8a7
devgithub723 <devs63849@gmail.com>
2024-09-27 15:40:57 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,18 +3,18 @@ var isValid = function(s) {
    
     let stack = [];
     
-    // Map to associate closing brackets with corresponding opening ones
+    
     const bracketMap = {
         ')': '(',
         '}': '{',
         ']': '['
     };
     
-    // Iterate through each character in the string
+    
     for (let char of s) {
-        // If it's a closing bracket
+        
         if (char in bracketMap) {
-            // Pop the top of the stack if it's not empty, otherwise assign a dummy value '#'
+           
             let topElement = stack.length === 0 ? '#' : stack.pop();
             
             // If the top element doesn't match the corresponding opening bracket, return false
